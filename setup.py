eaec0ce77520750d1b8f0564c2cc3aa5129de36c
MR-TAN <135997970+Tan-vai@users.noreply.github.com>
2024-08-02 09:48:47 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 import os, platform, time, sys
 print('\n\033[1;31m[\x1b[38;5;48m●\033[1;31m]\x1b[38;5;48m Checking For Update...')
 os.system('git pull --quiet 2>/dev/null')
+print('\x1b[38;5;208mNote \033[1;31m:\x1b[38;5;48m When setup is done, You see SetUp done msg')
 bit = platform.architecture()[0]
 if bit == '64bit':
- print('\033[1;31m[\x1b[38;5;48m●\033[1;31m]\x1b[38;5;48m 64 Bit Found')
- print('\033[1;31m━━━━━━━━━━━━━━━━\x1b[38;5;48m\n')
+ print('\033[1;31m[\x1b[38;5;48m●\033[1;31m]\x1b[38;5;48m You are 64 Bit user')
  time.sleep(3)
  os.system("clear") 
  import set
 elif bit == '32bit':
- print('\033[1;31m[\x1b[38;5;48m●\033[1;31m]\x1b[38;5;48m 32 Bit Found')
- print('\033[1;31m━━━━━━━━━━━━━━━━\x1b[38;5;48m\n')
+ print('\033[1;31m[\x1b[38;5;48m●\033[1;31m]\x1b[38;5;48m You are 32 Bit user')
  time.sleep(3)
  os.system("clear")
  import setup
